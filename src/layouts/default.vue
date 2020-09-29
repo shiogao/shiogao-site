09ffe11a03bc289597b108b15dc57cec687199a2
shiogao <github@shiogao.com>
2020-09-29 20:13:12 +0900

    delete header

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <anim-header />
-    <div style="width: 100vw; height: 60px"></div>
     <anim-hero v-if="$route.fullPath === '/'" />
     <nuxt />
     <anim-footer />
@@ -9,12 +7,10 @@
 </template>
 
 <script>
-import AnimHeader from "~/components/AnimHeader.vue";
 import AnimHero from "~/components/AnimHero.vue";
 import AnimFooter from "~/components/AnimFooter.vue";
 export default {
   components: {
-    AnimHeader,
     AnimHero,
     AnimFooter,
   },
