0330aea26cff0e28788c25f0b39c2d7181d0bdf8
shiogao <github@shiogao.com>
2020-12-26 13:08:26 +0900

    add article 2

--- src/contents/index.js
+++ src/contents/index.js
@@ -5,5 +5,11 @@ export const blogs = [
     title: "Webサイト公開",
     date: "2020-09-25",
     thumbnail: "shiogao"
+  },
+  {
+    id: "2",
+    title: "typescript-template",
+    date: "2020-12-26",
+    thumbnail: "shiogao"
   }
 ]
